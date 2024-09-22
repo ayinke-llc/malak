59e4c892150877f4e00522f0eafd6de9537ad3f6
Lanre Adelowo <yo@lanre.wtf>
2024-09-22 02:21:54 +0100

    add api to duplicate update

---
+++ server/update_actions.go
@@ -0,0 +1,80 @@
+package server
+
+import (
+	"context"
+	"errors"
+	"net/http"
+
+	"github.com/ayinke-llc/malak"
+	"github.com/ayinke-llc/malak/internal/pkg/util"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/render"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
+	"go.uber.org/zap"
+)
+
+// @Summary Duplicate a specific update
+// @Tags updates
+// @id duplicateContent
+// @Accept  json
+// @Produce  json
+// @Param reference path string required "update unique reference.. e.g update_"
+// @Param message body contentUpdateRequest true "update content body"
+// @Success 200 {object} createdUpdateResponse
+// @Failure 400 {object} APIStatus
+// @Failure 401 {object} APIStatus
+// @Failure 404 {object} APIStatus
+// @Failure 500 {object} APIStatus
+// @Router /workspaces/updates/{reference}/duplicate [post]
+func (u *updatesHandler) duplicate(
+	ctx context.Context,
+	span trace.Span,
+	logger *zap.Logger,
+	w http.ResponseWriter,
+	r *http.Request) (render.Renderer, Status) {
+
+	ref := chi.URLParam(r, "reference")
+
+	user := getUserFromContext(ctx)
+
+	span.SetAttributes(attribute.String("reference", ref))
+
+	logger = logger.With(zap.String("reference", ref))
+
+	logger.Debug("Duplicating update")
+
+	update, err := u.updateRepo.Get(ctx, malak.FetchUpdateOptions{
+		Reference: malak.Reference(ref),
+	})
+	if errors.Is(err, malak.ErrUpdateNotFound) {
+		return newAPIStatus(http.StatusNotFound,
+			"update does not exists"), StatusFailed
+	}
+
+	if err != nil {
+		logger.Error("could not fetch update", zap.Error(err))
+		return newAPIStatus(http.StatusInternalServerError,
+			"an error occurred while fetching update"), StatusFailed
+	}
+
+	newUpdate := &malak.Update{
+		Content:     update.Content,
+		Reference:   u.referenceGenerator.Generate(malak.EntityTypeUpdate),
+		CreatedBy:   user.ID,
+		Status:      malak.UpdateStatusDraft,
+		WorkspaceID: update.WorkspaceID,
+	}
+
+	if err := u.updateRepo.Create(ctx, newUpdate); err != nil {
+		logger.Error("could not create updates", zap.Error(err))
+
+		return newAPIStatus(http.StatusInternalServerError,
+			"could not create updates"), StatusFailed
+	}
+
+	return createdUpdateResponse{
+		Update:    util.DeRef(newUpdate),
+		APIStatus: newAPIStatus(http.StatusCreated, "update has been duplicated"),
+	}, StatusSuccess
+}
