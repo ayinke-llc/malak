96af1321d639c9688400b025c14163a3c0fc4699
Lanre Adelowo <yo@lanre.wtf>
2024-09-22 03:23:17 +0100

    add posthog js library

--- web/ui/environment.d.ts
+++ web/ui/environment.d.ts
@@ -6,6 +6,8 @@ declare global {
       NEXT_PUBLIC_GOOGLE_CLIENT_ID: string;
       NEXT_PUBLIC_MALAK_TERMS_CONDITION_LINK: string
       NEXT_PUBLIC_MALAK_PRIVACY_POLICY_LINK: string
+      // use posthog to track analytics or not
+      NEXT_PUBLIC_ENABLE_POSTHOG: boolean
     }
   }
 }
