4cfbd01a69430c661f33f2a7d3313c1abab578d9
Lanre Adelowo <yo@lanre.wtf>
2024-09-22 13:36:49 +0100

    set up posthog for analytics and make it configurable

--- web/ui/environment.d.ts
+++ web/ui/environment.d.ts
@@ -7,7 +7,9 @@ declare global {
       NEXT_PUBLIC_MALAK_TERMS_CONDITION_LINK: string
       NEXT_PUBLIC_MALAK_PRIVACY_POLICY_LINK: string
       // use posthog to track analytics or not
-      NEXT_PUBLIC_ENABLE_POSTHOG: boolean
+      NEXT_PUBLIC_MALAK_ENABLE_POSTHOG: boolean
+      NEXT_PUBLIC_MALAK_POSTHOG_KEY: string
+      NEXT_PUBLIC_MALAK_POSTHOG_HOST: string
     }
   }
 }
