bc08f660cedff65b93ed7b1d01a2bf019d98de56
Lanre Adelowo <yo@lanre.wtf>
2024-09-28 21:55:11 +0100

    format

--- web/ui/src/lib/config.ts
+++ web/ui/src/lib/config.ts
@@ -6,13 +6,14 @@ const MALAK_PRIVACY_POLICY_LINK =
   process.env.NEXT_PUBLIC_MALAK_PRIVACY_POLICY_LINK ||
   "https://ayinke.ventures/malak/privacy";
 
-const MALAK_SENTRY_ENABLED = process.env.NEXT_PUBLIC_SENTRY_DSN.length > 0 ? true : false
-const MALAK_SENTRY_DSN = process.env.NEXT_PUBLIC_SENTRY_DSN
+const MALAK_SENTRY_ENABLED =
+  process.env.NEXT_PUBLIC_SENTRY_DSN.length > 0 ? true : false;
+const MALAK_SENTRY_DSN = process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 export {
   GOOGLE_CLIENT_ID,
   MALAK_TERMS_CONDITION_LINK,
   MALAK_PRIVACY_POLICY_LINK,
   MALAK_SENTRY_DSN,
-  MALAK_SENTRY_ENABLED
+  MALAK_SENTRY_ENABLED,
 };
