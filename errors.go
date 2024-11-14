7fd0dd06426a492e60e5ce460852cf15c0207d26
Lanre Adelowo <yo@lanre.wtf>
2024-11-14 14:30:25 +0100

    fix test

--- errors.go
+++ errors.go
@@ -7,5 +7,9 @@ type MalakError string
 func (m MalakError) Error() string { return string(m) }
 
 func IsDuplicateUniqueError(e error) bool {
+	if e == nil {
+		return false
+	}
+
 	return strings.Contains(e.Error(), "duplicate key value violates unique constraint")
 }
