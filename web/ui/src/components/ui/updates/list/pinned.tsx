2aa94c18dcb38c4993395a3b2a3216d1427540ab
Lanre Adelowo <yo@lanre.wtf>
2024-11-16 21:32:27 +0100

    unpin request

--- web/ui/src/components/ui/updates/list/pinned.tsx
+++ web/ui/src/components/ui/updates/list/pinned.tsx
@@ -1,12 +1,12 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { Card, CardContent } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
-import { RiCalendarLine, RiFlipHorizontal2Line, RiFlipHorizontalLine, RiMoreLine, RiPushpinLine, RiUnpinLine } from "@remixicon/react"
-import { MalakUpdate } from "@/client/Api"
+import { RiCalendarLine, RiMoreLine } from "@remixicon/react"
+import { MalakUpdate, ServerAPIStatus, ServerCreatedUpdateResponse, } from "@/client/Api"
 import Link from "next/link"
-import UpdateBadge from "../../custom/update/badge"
+import type { AxiosError, AxiosResponse } from "axios";
 import Skeleton from "../../custom/loader/skeleton"
 import { useMutation, useQuery } from "@tanstack/react-query"
-import { LIST_PINNED_UPDATES } from "@/lib/query-constants"
+import { LIST_PINNED_UPDATES, TOGGLE_PINNED_STATE } from "@/lib/query-constants"
 import client from "@/lib/client"
 import { toast } from "sonner";
 import { format } from "date-fns"
@@ -17,6 +17,9 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { EVENT_TOGGLE_PINNED_STATE } from "@/lib/analytics-constansts"
+import { usePostHog } from "posthog-js/react"
+import { useState } from "react"
 
 const PinnedList = () => {
 
@@ -58,11 +61,44 @@ const PinnedList = () => {
 
 const Item = (update: MalakUpdate) => {
 
-  const togglePinnedStatus = (reference: string) => {
+  const posthog = usePostHog()
+
+  const [loading, setLoading] = useState<boolean>(false);
+  const [deleted, setDeleted] = useState<boolean>(false);
+
+  const togglePinnedStatusMutation = useMutation({
+    mutationKey: [TOGGLE_PINNED_STATE],
+    retry: false,
+    gcTime: Number.POSITIVE_INFINITY,
+    onMutate: () => {
+      posthog?.capture(EVENT_TOGGLE_PINNED_STATE);
+      setLoading(true)
+    },
+    onSettled: () => setLoading(false),
+    mutationFn: (reference: string) =>
+      client.workspaces.toggleUpdatePin(reference),
+    onError(err: AxiosError<ServerAPIStatus>) {
+      let msg = err.message;
+      if (err.response !== undefined) {
+        msg = err.response.data.message;
+      }
+      toast.error(msg);
+    },
+    onSuccess: (resp: AxiosResponse<ServerCreatedUpdateResponse>) => {
+      toast.success(`Update has been Unpinned`);
+      setDeleted(resp.data?.update?.is_pinned as boolean)
+    },
+  });
+
+  if (deleted) {
+    return null
+  }
+
+  if (loading) {
+    return <Skeleton count={5} />
   }
 
   return (
-
     <Card key={update?.id as string} className="bg-white border-none shadow-md hover:shadow-lg transition-shadow duration-300">
       <CardContent className="p-4">
         <div className="flex items-start justify-between mb-3">
@@ -77,7 +113,7 @@ const Item = (update: MalakUpdate) => {
               </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end">
-              <DropdownMenuItem onClick={() => { }}>
+              <DropdownMenuItem onClick={() => togglePinnedStatusMutation.mutate(update?.reference as string)}>
                 Unpin
               </DropdownMenuItem>
               <DropdownMenuItem>
