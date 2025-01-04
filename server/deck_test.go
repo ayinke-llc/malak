a86b64fd8d86eaa33cd5ea58ab41e4206a0f5cd2
Lanre Adelowo <yo@lanre.wtf>
2025-01-04 23:38:51 +0100

    add tests

--- server/deck_test.go
+++ server/deck_test.go
@@ -383,3 +383,152 @@ func TestDeckHandler_Fetch(t *testing.T) {
 		})
 	}
 }
+
+func generateDeckUpdatePreferencesRequest() []struct {
+	name               string
+	mockFn             func(deck *malak_mocks.MockDeckRepository)
+	expectedStatusCode int
+	req                updateDeckPreferencesRequest
+} {
+	return []struct {
+		name               string
+		mockFn             func(deck *malak_mocks.MockDeckRepository)
+		expectedStatusCode int
+		req                updateDeckPreferencesRequest
+	}{
+		{
+			name:               "no reference provided",
+			mockFn:             func(deck *malak_mocks.MockDeckRepository) {},
+			expectedStatusCode: http.StatusBadRequest,
+			req:                updateDeckPreferencesRequest{},
+		},
+		{
+			name:               "password enabled but no password provided",
+			mockFn:             func(deck *malak_mocks.MockDeckRepository) {},
+			expectedStatusCode: http.StatusBadRequest,
+			req: updateDeckPreferencesRequest{
+				PasswordProtection: struct {
+					Enabled bool           `json:"enabled,omitempty"`
+					Value   malak.Password `json:"value,omitempty"`
+				}{
+					Enabled: true,
+				},
+			},
+		},
+		{
+			name: "could not fetch deck",
+			mockFn: func(deck *malak_mocks.MockDeckRepository) {
+				deck.EXPECT().Get(gomock.Any(), gomock.Any()).
+					Times(1).
+					Return(&malak.Deck{}, errors.New("error occurred"))
+			},
+			expectedStatusCode: http.StatusInternalServerError,
+			req: updateDeckPreferencesRequest{
+				EnableDownloading: true,
+				RequireEmail:      true,
+			},
+		},
+		{
+			name: "deck not found",
+			mockFn: func(deck *malak_mocks.MockDeckRepository) {
+				deck.EXPECT().Get(gomock.Any(), gomock.Any()).
+					Times(1).
+					Return(&malak.Deck{}, malak.ErrDeckNotFound)
+			},
+			expectedStatusCode: http.StatusNotFound,
+			req: updateDeckPreferencesRequest{
+				EnableDownloading: true,
+				RequireEmail:      true,
+			},
+		},
+		{
+			name: "could not update preferences",
+			mockFn: func(deck *malak_mocks.MockDeckRepository) {
+				deck.EXPECT().Get(gomock.Any(), gomock.Any()).
+					Times(1).
+					Return(&malak.Deck{
+						Reference:      "deck_test",
+						DeckPreference: &malak.DeckPreference{},
+					}, nil)
+
+				deck.EXPECT().UpdatePreferences(gomock.Any(), gomock.Any()).
+					Times(1).
+					Return(errors.New("error occurred"))
+			},
+			expectedStatusCode: http.StatusInternalServerError,
+			req: updateDeckPreferencesRequest{
+				EnableDownloading: true,
+				RequireEmail:      true,
+			},
+		},
+		{
+			name: "successfully updated preferences",
+			mockFn: func(deck *malak_mocks.MockDeckRepository) {
+				deck.EXPECT().Get(gomock.Any(), gomock.Any()).
+					Times(1).
+					Return(&malak.Deck{
+						Reference:      "deck_test",
+						DeckPreference: &malak.DeckPreference{},
+					}, nil)
+
+				deck.EXPECT().UpdatePreferences(gomock.Any(), gomock.Any()).
+					Times(1).
+					Return(nil)
+			},
+			expectedStatusCode: http.StatusOK,
+			req: updateDeckPreferencesRequest{
+				EnableDownloading: true,
+				RequireEmail:      true,
+				PasswordProtection: struct {
+					Enabled bool           `json:"enabled,omitempty"`
+					Value   malak.Password `json:"value,omitempty"`
+				}{
+					Enabled: true,
+					Value:   "secure_password",
+				},
+			},
+		},
+	}
+}
+
+func TestDeckHandler_UpdatePreferences(t *testing.T) {
+	for _, v := range generateDeckUpdatePreferencesRequest() {
+		t.Run(v.name, func(t *testing.T) {
+			controller := gomock.NewController(t)
+			defer controller.Finish()
+
+			deckRepo := malak_mocks.NewMockDeckRepository(controller)
+			v.mockFn(deckRepo)
+
+			u := &deckHandler{
+				referenceGenerator: &mockReferenceGenerator{},
+				deckRepo:           deckRepo,
+			}
+
+			var b = bytes.NewBuffer(nil)
+			err := json.NewEncoder(b).Encode(v.req)
+			require.NoError(t, err)
+
+			rr := httptest.NewRecorder()
+			req := httptest.NewRequest(http.MethodPut, "/", b)
+			req.Header.Add("Content-Type", "application/json")
+
+			req = req.WithContext(writeUserToCtx(req.Context(), &malak.User{}))
+			req = req.WithContext(writeWorkspaceToCtx(req.Context(), &malak.Workspace{}))
+
+			ctx := chi.NewRouteContext()
+			if v.name != "no reference provided" {
+				ctx.URLParams.Add("reference", "deck_test")
+			}
+			req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, ctx))
+
+			WrapMalakHTTPHandler(getLogger(t),
+				u.updatePreferences,
+				getConfig(), "decks.update_preferences").
+				ServeHTTP(rr, req)
+
+			require.Equal(t, v.expectedStatusCode, rr.Code)
+			verifyMatch(t, rr)
+		})
+	}
+}
