e70dfb4c6700b63f0abbc7c10085c949c4d9b2a8
Lanre Adelowo <yo@lanre.wtf>
2025-01-23 12:07:02 +0100

    fixes table

--- web/ui/src/components/ui/updates/analytics/view.tsx
+++ web/ui/src/components/ui/updates/analytics/view.tsx
@@ -54,11 +54,11 @@ const columns: ColumnDef<MalakUpdateRecipient>[] = [
     accessorKey: "delivered",
     header: "Delivered",
     cell: ({ row }) => (
-      <div className="flex justify-center">
+      <div className="flex justify-right">
         {row.original.update_recipient_stat?.is_delivered ? (
-          <span className="text-green-500 text-xl">✓</span>
+          "✅"
         ) : (
-          <span className="text-red-500 text-xl">✗</span>
+          "❌"
         )}
       </div>
     ),
@@ -78,13 +78,8 @@ const columns: ColumnDef<MalakUpdateRecipient>[] = [
     accessorKey: "reacted",
     header: "Reacted",
     cell: ({ row }) => (
-      <div className="text-center text-xl">{row.original.update_recipient_stat?.has_reaction || "-"}</div>
+      <div className="text-left text-xl">{row.original.update_recipient_stat?.has_reaction ? "👍" : "❌"}</div>
     ),
-  },
-  {
-    accessorKey: "clicks",
-    header: "Clicks",
-    cell: () => <div className="text-center">{0}</div>,
   },
 ]
 
