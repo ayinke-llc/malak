a9f10e055c1800839104becd3a06a2ce8d04cfb6
Lanre Adelowo <yo@lanre.wtf>
2025-03-04 19:41:58 +0100

    config

--- web/ui/next.config.mjs
+++ web/ui/next.config.mjs
@@ -2,6 +2,9 @@ import { withSentryConfig } from "@sentry/nextjs";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
   redirects: async () => {
     return [
       {
