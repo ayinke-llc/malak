f00c53571e57c07fa6033563b02ad4bc0fb20b65
Lanre Adelowo <yo@lanre.wtf>
2025-03-13 18:03:14 +0100

    fix copy

--- web/ui/src/components/pages/login.tsx
+++ web/ui/src/components/pages/login.tsx
@@ -108,7 +108,7 @@ export default function LoginPage() {
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">Welcome back</h1>
               <p className="text-sm text-muted-foreground">
-                Sign in to your account to continue
+                Sign into or create your account to continue
               </p>
             </div>
             <div className="grid gap-4">
